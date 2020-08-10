fb6aee21805a5c2d538950ab2c6ba74e5f360798
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-08-10 21:00:46 +0300

    Added menu, list , sidebar components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu.js'
+import List from './components/complicated_list.js'
+import Sidebar from './components/sidebar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <List />
+      <Sidebar />
     </div>
-  );
+  )
 }
 
 export default App;
