b3bfcbb3097b304daf81d60487ce6485f3d65d8d
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-08-11 12:20:45 +0300

    imported compiled bootstrap styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Menu from './components/menu.js';
 import List from './components/complicated_list.js';
 import Sidebar from './components/sidebar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <Menu />
+      <div className="container">
+          <div className="row">
+              <div className="col-12">
+                <Menu />
+              </div>
+          </div>
+      </div>
       <List />
       <Sidebar />
     </div>
