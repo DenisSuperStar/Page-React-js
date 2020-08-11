94bc8e5541ed9b656939ae9346062901db1e063c
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-08-11 18:54:30 +0300

    added flexbox markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss'
 import Menu from './components/menu.js';
 import List from './components/complicated_list.js';
 import Sidebar from './components/sidebar.js';
@@ -8,15 +8,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
-      <div className="container">
+      <div className="container app-content">
           <div className="row">
               <div className="col-12">
                 <Menu />
               </div>
           </div>
+          <div className="main">
+            <div className="main__general-content">
+              <List />
+            </div>
+            <div className="main__sidebar-content">
+              <Sidebar />
+            </div>
+          </div>
       </div>
-      <List />
-      <Sidebar />
     </div>
   )
 }
