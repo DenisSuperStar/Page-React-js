42d272cce1bf8ab671436ea966d7c5f36e5dba7b
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-08-14 17:54:38 +0300

    created mobile-menu components

---
+++ src/components/mobile_menu.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import '../components_styles/mobile_menu.scss'
+
+class MobileMenu extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            item: [
+                {id: 0, link: 'Обмен'},
+                {id: 1, link: 'Бонусы'},
+                {id: 2, link: 'Заработок'},
+                {id: 3, link: 'FAQ'},
+                {id: 4, link: 'Связаться с поддержкой'}
+            ]
+        }
+    }
+    render() {
+        const RENDER_MOBILE_MENU_ITEMS = this.state.item.map(item => {
+            return (
+                <li className="menu-box__item-list" key={item.id}>
+                    <a 
+                    href="#" 
+                    className="
+                        menu-box__item-link 
+                        menu-box_link_sizes 
+                        menu-box_link_colored
+                    "
+                    >
+                        {item.link}
+                    </a>
+                </li> 
+            )
+        })
+        return (
+            <div>
+                <div className="humburger-menu">
+                    <input type="checkbox" id="menu__toggle" className="humburger-menu__toggle" />
+                    <label 
+                        className="
+                            humburger-menu__btn 
+                            humburger-menu_sizes 
+                            humburger-menu_moved
+                            humburger-menu_colored
+                        " 
+                        htmlFor="menu__toggle">
+                        <span></span>
+                    </label>
+                    
+                    <ul 
+                        className="
+                            menu-box 
+                            menu-box_moved 
+                            menu-box_sizes 
+                            menu-box_outer_dists 
+                            menu-box_colored_shadow
+                        "
+                        >
+                            {RENDER_MOBILE_MENU_ITEMS}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MobileMenu;
