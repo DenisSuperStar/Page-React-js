b97aebf2d7990c8fe4574ff8aadd0d385e2ee846
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-08-14 18:02:42 +0300

    created block of show menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import MenuLeft from './menu_item_left.js';
+import MobileMenu from './mobile_menu.js'
+import NavbarLeft from './navbar-left.js'
 import NavbarRight from './navbar-right.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components_styles/menu.scss';
 
 class Menu extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            menu_left: [
-                {id: 0, link: "Обмен"},
-                {id: 1, link: "Бонусы"},
-                {id: 2, link: "Заработок"},
-                {id: 3, link: "FAQ"},
-                {id: 4, link: 'Связаться с поддержкой'}
-            ]
-        }
-    }
     render() {
-        const RENDER_MENU_ITEMS = this.state.menu_left.map(item => {
-            return <MenuLeft link={item.link} key={item.id} />
-        })
         return (
             <div>
                 <nav className="navbar-container">
-                    <div className="navbar-left">
-                        <ul className="menu-left menu-left_remove_list-styles">
-                            <a className="menu-left__navbar-brand" href="#">NavBar</a>
-                            {RENDER_MENU_ITEMS}
-                        </ul>
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <div id="menu__shown">
+                        <NavbarLeft />
+                        <NavbarRight />
                     </div>
-                    <NavbarRight />
+                    <div id="menu__hidden">
+                        <MobileMenu />
+                    </div>
                 </nav>
             </div>
         )
